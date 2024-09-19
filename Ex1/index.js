79a370a73a41692cef83bfc45b3bdd13038237b4
MathisGau <126653891+MathisGau@users.noreply.github.com>
2024-09-19 09:58:58 +0200

    Ex1 terminer

--- Ex1/index.js
+++ Ex1/index.js
@@ -5,6 +5,15 @@
  * La fonction doit retourner "Fizz" pour les multiples de 3, "Buzz" pour les multiples de 5,
  * et "FizzBuzz" pour les multiples de 3 et 5.
  */
-function fizzBuzz(number) {}
+function fizzBuzz(number) {
+  if (number % 3 === 0 && number % 5 === 0) {
+    return "FizzBuzz";
+  } else if (number % 3 === 0) {
+    return "Fizz";
+  } else if (number % 5 === 0) {
+    return "Buzz";
+  }
+  return number;
+}
 
 module.exports = { fizzBuzz };
--- Ex1/index.test.js
+++ Ex1/index.test.js
@@ -1,13 +1,16 @@
-const { fizzBuzz } = require("./index.js")
+const { fizzBuzz } = require("./index.js");
 
-//Ecrire le test pour les multiples de 5
+//Ecrire le test pour les multiples de 3
 test('retourne "Fizz" pour les multiples de 3', () => {
+  expect(fizzBuzz(3)).toBe("Fizz");
 });
 
 //Ecrire le test pour les multiples de 5
 test('retourne "Buzz" pour les multiples de 5', () => {
+  expect(fizzBuzz(5)).toBe("Buzz");
 });
 
 //Ecrire le test pour les multiples de 3 & 5
 test('retourne "FizzBuzz" pour les multiples de 3 et 5', () => {
+  expect(fizzBuzz(15)).toBe("FizzBuzz");
 });
