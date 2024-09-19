a484bcbc1de71dcfc954b123e0e0871c8a2bf135
MathisGau <126653891+MathisGau@users.noreply.github.com>
2024-09-19 11:00:27 +0200

    Ex2 terminer

--- Ex2/index.js
+++ Ex2/index.js
@@ -1,8 +1,17 @@
-// Objectif : Écrire une fonction qui valide si un mot de passe respecte certains critères 
+// Objectif : Écrire une fonction qui valide si un mot de passe respecte certains critères
 // Les critères : Minimum 8 caractères, un chiffre , une majuscule minimum, une minuscule minimum
 
 function isValidPassword(password) {
-
+  if (
+    password.length < 8 ||
+    !password.match(/[0-9]/) ||
+    !password.match(/[A-Z]/) ||
+    !password.match(/[a-z]/)
+  ) {
+    return false;
+  } else {
+    return true;
+  }
 }
 
-module.exports = { isValidPassword }
+module.exports = { isValidPassword };
--- Ex2/index.test.js
+++ Ex2/index.test.js
@@ -1,7 +1,17 @@
-const { isValidPassword } = require("./index.js")
+const { isValidPassword } = require("./index.js");
 
 /*
  * Instructions :
  * Créer des tests pour des mots de passe valides et invalides.
  * Introduire des tests paramétrés pour valider plusieurs mots de passe dans une seule fonction.
-*/
+ */
+
+test.each([
+  [true, "Password1"],
+  [false, "password1"],
+  [false, "Password"],
+  [false, "Pass1"],
+  [false, "0123456789"],
+])("retourne %o pour le mdp %s", (expected, password) => {
+  expect(isValidPassword(password)).toBe(expected);
+});
