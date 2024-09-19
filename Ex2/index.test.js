dbe1f7108fe32984001e5bbd8353675d30a3e224
Bryan LEVIEUX <bryan@agence-webup.com>
2024-09-19 09:52:25 +0200

    Ajout de l'exercice 2

---
+++ Ex2/index.test.js
@@ -0,0 +1,7 @@
+const { isValidPassword } = require("./index.js")
+
+/*
+ * Instructions :
+ * Créer des tests pour des mots de passe valides et invalides.
+ * Introduire des tests paramétrés pour valider plusieurs mots de passe dans une seule fonction.
+*/
