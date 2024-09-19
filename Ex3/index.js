9194be92540d6018f34b563cd2c677f6bd560515
MathisGau <126653891+MathisGau@users.noreply.github.com>
2024-09-19 11:28:37 +0200

    Ex3 terminer

--- Ex3/index.js
+++ Ex3/index.js
@@ -2,34 +2,43 @@
 // (addition, soustraction, multiplication, division) avec des tests pour chaque fonction.
 
 function addition(firstNumber, secondNumber) {
-
+  return firstNumber + secondNumber;
 }
 
 function soustraction(firstNumber, secondNumber) {
-
+  return firstNumber - secondNumber;
 }
 
 function multiplication(firstNumber, secondNumber) {
-
+  return firstNumber * secondNumber;
 }
 
 function division(firstNumber, secondNumber) {
-
+  return firstNumber / secondNumber;
 }
 
 /**
- * @param {number} firstNumber 
- * @param {number} secondNumber 
- * @param {string} operation peut être +, -, * ou / 
+ * @param {number} firstNumber
+ * @param {number} secondNumber
+ * @param {string} operation peut être +, -, * ou /
  */
-function calculate(firstNumber, secondNumber, operation) {
-
+function calculate(firstNumber, operation, secondNumber) {
+  switch (operation) {
+    case "+":
+      return addition(firstNumber, secondNumber);
+    case "-":
+      return soustraction(firstNumber, secondNumber);
+    case "*":
+      return multiplication(firstNumber, secondNumber);
+    case "/":
+      return division(firstNumber, secondNumber);
+  }
 }
 
 module.exports = {
-    addition,
-    soustraction,
-    multiplication,
-    division,
-    calculate,
-}
+  addition,
+  soustraction,
+  multiplication,
+  division,
+  calculate,
+};
--- Ex3/index.test.js
+++ Ex3/index.test.js
@@ -1,6 +1,24 @@
-const { isValidPassword } = require("./index.js")
+const {
+  calculate,
+  addition,
+  soustraction,
+  multiplication,
+  division,
+} = require("./index.js");
 
 /*
  * Instructions :
  * Créer des tests pour addition, soustraction, multiplication, division et calculate.
-*/
+ */
+
+test.each([
+  [3, "+", 5, 8],
+  [3, "-", 5, -2],
+  [3, "*", 5, 15],
+  [3, "/", 5, 0.6],
+])(
+  "%f %s %f est égal à %f",
+  (firstNumber, operation, secondNumber, expected) => {
+    expect(calculate(firstNumber, operation, secondNumber)).toBe(expected);
+  }
+);
